f2d140ef3a9d5a65e6fc72bd0863e34c1883fe38
MKondrashin <kondrashin.mv@gmail.com>
2016-10-22 14:43:18 +0300

    Changed class name to fit java coding conventions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
@@ -1,10 +1,10 @@
 package ru.stqa.pft.sandbox;
 
-public class hello
+public class Hello
 {
 	public static void main(String[] args)
-{
-	System.out.println("Hello world");
-}
+	{
+		System.out.println("Hello world");
+	}
 
 }
