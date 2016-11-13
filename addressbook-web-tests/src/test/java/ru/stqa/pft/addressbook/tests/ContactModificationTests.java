2a4e9259205b17506cbf82f40ce05fa4a2ff8a7d
MKondrashin <kondrashin.mv@gmail.com>
2016-11-13 15:51:55 +0300

    task10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -21,12 +23,26 @@ public class ContactModificationTests extends TestBase {
             app.getContactHelper().createContact(ContactData.getRandomValidContactData());
             app.getNavigationHelper().goToHomePage();
         }
+
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().modifyContact(ContactData.getRandomValidContactData(),before.size() - 1);
+
+        ContactData contact = ContactData.getRandomValidContactData();
+        app.getContactHelper().modifyContact(contact, before.size() - 1);
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
 
         Assert.assertEquals(before.size(), after.size());
+
+        Comparator<? super ContactData> byId = (c1, c2)  -> Integer.compare(c1.getId(),c2.getId());
+        before.remove(before.size() - 1);
+        contact.setId(after.stream().max(byId).get().getId());
+        before.add(contact);
+
+
+        before.sort(byId);
+        after.sort(byId);
+
+        Assert.assertEquals(after,before);
 
     }
 
