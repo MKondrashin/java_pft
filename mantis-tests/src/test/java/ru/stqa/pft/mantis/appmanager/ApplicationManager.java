929565549da94637316fc39ad00e415479819786
MKondrashin <kondrashin.mv@gmail.com>
2016-12-17 13:50:05 +0300

    Добавлен нвый модуль для mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,59 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by User on 30.10.2016.
+ */
+public class ApplicationManager {
+
+    WebDriver wd;
+    private String browser;
+    private final Properties properties;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+        properties = new Properties();
+
+    }
+
+
+    public void init() throws IOException {
+
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+        if(browser.equals(BrowserType.FIREFOX)){
+            System.setProperty("webdriver.gecko.driver",properties.getProperty("web.pathToFirefoxDriver"));
+            wd = new FirefoxDriver();
+        } else if(browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver",properties.getProperty("web.pathToChromeDriver"));
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE))
+        {
+            System.setProperty("webdriver.ie.driver",properties.getProperty("web.pathToIeDriver"));
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));// "http://localhost/addressbook");
+
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+}
