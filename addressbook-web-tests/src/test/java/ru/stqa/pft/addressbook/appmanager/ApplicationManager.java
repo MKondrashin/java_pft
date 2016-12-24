1d08c0edd20d5963397885c5d315ce31890163ce
MKondrashin <kondrashin.mv@gmail.com>
2016-12-25 01:21:15 +0300

    selenium server added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -42,18 +46,23 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
-        if(browser.equals(BrowserType.FIREFOX)){
-            System.setProperty("webdriver.gecko.driver",properties.getProperty("web.pathToFirefoxDriver"));
-            wd = new FirefoxDriver();
-        } else if(browser.equals(BrowserType.CHROME)) {
-            System.setProperty("webdriver.chrome.driver",properties.getProperty("web.pathToChromeDriver"));
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE))
-        {
-            System.setProperty("webdriver.ie.driver",properties.getProperty("web.pathToIeDriver"));
-            wd = new InternetExplorerDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("web.pathToFirefoxDriver"));
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("web.pathToChromeDriver"));
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                System.setProperty("webdriver.ie.driver", properties.getProperty("web.pathToIeDriver"));
+                wd = new InternetExplorerDriver();
+            }
         }
-
+        else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+        }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
